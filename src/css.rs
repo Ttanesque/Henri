a6bd6d9660206cc24e711144e6e2292f5b436871
Hugo Porchet <hugo.porchet.belmonte@gmail.com>
2023-12-10 16:33:08 +0100

    feat: impl Display

--- src/css.rs
+++ src/css.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
 
 pub type Combinator = (Box<CSSCombinator>, Box<CSSCombinator>);
 pub type StrPair = (String, String);
@@ -11,6 +11,7 @@ pub enum CSSCombinator {
     Neightbour(Combinator),
     DirectChild(Combinator),
     Child(Combinator),
+    List(Vec<CSSCombinator>),
 }
 
 #[derive(Debug)]
@@ -94,6 +95,71 @@ impl CSSword {
             attr: None,
             psd_class: Vec::new(),
             psd_elt: Vec::new(),
+        }
+    }
+}
+
+impl Display for CSSword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut result = String::new();
+
+        // ajout du tag pas de préfix
+        let tag = self.tag.clone().unwrap_or("".to_string());
+        result.push_str(tag.as_str());
+        result.push(' ');
+
+        // id à un préfix on dois check
+        let id = self.id.clone().unwrap_or("".to_string());
+        if id != "" {
+            result.push_str(format!("#{} ", id).as_str());
+        }
+
+        // pareil pour les attr
+        let attr = self.attr.clone().unwrap_or("".to_string());
+        if attr != "" {
+            result.push_str(format!("[{}] ", attr).as_str());
+        }
+
+        // ajout de listes
+        let class = self.class.join(".");
+        if class != "" {
+            result.push_str(format!(".{} ", class).as_str());
+        }
+
+        let psd_class = self.psd_class.join(":");
+        if psd_class != "" {
+            result.push_str(format!(":{} ", psd_class).as_str());
+        }
+
+        let psd_elt = self.psd_elt.join("::");
+        if psd_elt != "" {
+            result.push_str(format!("::{} ", psd_elt).as_str());
+        }
+
+        // nettoie les espaces
+        let words: Vec<_> = result.split_whitespace().collect();
+
+        write!(f, "{}", words.join(" "))
+    }
+}
+
+impl Display for CSSCombinator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CSSCombinator::Unit(selec) => write!(f, "{}", selec),
+            CSSCombinator::DirectNeightbour((left, right)) => write!(f, "{} + {}", left, right),
+            CSSCombinator::Neightbour((left, right)) => write!(f, "{} ~ {}", left, right),
+            CSSCombinator::DirectChild((left, right)) => write!(f, "{} > {}", left, right),
+            CSSCombinator::Child((left, right)) => write!(f, "{} {}", left, right),
+            CSSCombinator::List(list) => {
+                let mut formatted_list = String::new();
+                for i in list {
+                    formatted_list.push_str(format!("{},", i).as_str());
+                }
+
+                write!(f, "{}", formatted_list)
+            }
+            CSSCombinator::None => write!(f, ""),
         }
     }
 }
